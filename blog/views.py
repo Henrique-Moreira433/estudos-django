7839ed70929a100b371fa1b7f59a23212b1c703a
Henrique Moreira <henriquemoreiranunes433@gmail.com>
2026-05-25 17:17:37 -0300

    usando context

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render
 
 def blog(request):
     print('blog')
+    context ={ 
+    'text': 'ESTAMOS NO BLOG '
+    }
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context
     )
 
 def exemplo(request):
     print('exemplo')
+    context ={ 
+    'text': 'ESTAMOS NO EXEMPLO ',
+    'title': 'titulo meu',
+    }
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        
+        context
     )
--- home/views.py
+++ home/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 
 def home(request):
     print('home')
+    context ={ 
+    'text': 'ESTAMOS NA HOME '
+    }
+
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context
+        
     )
